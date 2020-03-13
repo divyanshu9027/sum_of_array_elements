593cdbc0e90511567adb4f1cdc46c167f51087a3
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-13 08:12:29 +0530

    take the size of an array from the user

--- src/solution/Main.java
+++ src/solution/Main.java
@@ -5,5 +5,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int sum = 0;
+
     }
 }
